e754be4fafcfc612283223f71cc39b9bb82ea610
unknown <krishkarthik111@gmail.com>
2022-09-28 21:22:42 +0530

    Initial commit

---
+++ my-app/src/app/message.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MessageService {
+  messages1: string[] = [];
+
+  add(message: string) {
+    this.messages1.push(message);
+  }
+
+  clear() {
+    this.messages1 = [];
+  }
+}
